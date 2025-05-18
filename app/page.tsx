33651368c0d229870fdea19e4bf2a8fc8c02036d
jonte265 <200072657+jonte265@users.noreply.github.com>
2025-05-18 22:44:54 +0200

    current week and if friday

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,16 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
+import {
+  isFriday,
+  format,
+  addDays,
+  formatDistance,
+  formatRelative,
+  subDays,
+} from 'date-fns';
+import { sv } from 'date-fns/locale';
+import { useEffect, useState } from 'react';
 import {
   Card,
   CardContent,
@@ -7,24 +19,29 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import InfoCard from '@/components/custom/InfoCard';
 
 export default function Home() {
+  const date = new Date();
+
+  const [isItFriday, setIsItFriday] = useState(false);
+
+  useEffect(() => {
+    setIsItFriday(isFriday(date));
+  }, []);
+
   return (
     <main className='p-4'>
-      <h1 className=''>Week</h1>
-      <Button>Click me</Button>
-      <Card>
-        <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
-      </Card>
+      <div className='grid gap-4'>
+        <InfoCard
+          infoTitle='Är det fredag?'
+          infoHeading={isItFriday ? 'JA' : 'NEJ'}
+          infoDesc={`Idag är det ${format(date, 'eeee', { locale: sv })}`}
+        />
+
+        <InfoCard infoTitle={`V: ${format(date, 'w', { locale: sv })}`} />
+      </div>
+      <p>{format(date, 'yyyy-MM-dd HH:mm')}</p>
     </main>
   );
 }
---
+++ components/custom/InfoCard.tsx
@@ -0,0 +1,18 @@
+type infoCardType = {
+  infoTitle: any;
+  infoHeading?: any;
+  infoDesc?: any;
+};
+
+function InfoCard({ infoTitle, infoHeading, infoDesc }: infoCardType) {
+  return (
+    <div className='border-2 rounded-2xl p-4 flex flex-col items-center justify-center gap-2'>
+      <h2 className='text-2xl font-bold'>{infoTitle}</h2>
+      <h3 className='text-xl font-semibold'>{infoHeading}</h3>
+
+      <p className='text-muted-foreground'>{infoDesc}</p>
+    </div>
+  );
+}
+
+export default InfoCard;
