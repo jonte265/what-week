5025e1d07c5da7c8d01b55b13ffdc6ebac5b321e
jonte265 <200072657+jonte265@users.noreply.github.com>
2025-05-18 20:55:27 +0200

    add dark mode

---
+++ components/custom/HeaderNav.tsx
@@ -0,0 +1,12 @@
+import { ModeToggle } from '../ui/ModeToggle';
+
+function HeaderNav() {
+  return (
+    <header>
+      <h1>Vilken Vecka</h1>
+      <ModeToggle />
+    </header>
+  );
+}
+
+export default HeaderNav;
